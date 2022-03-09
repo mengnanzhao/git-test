4059197d6ed45e1131c29d118d97c7423813a4fa
zmn <757052424@qq.com>
2022-03-09 16:24:42 +0800

    push commit

--- src/main/java/com/zmn/atguigu/git/GitTest.java
+++ src/main/java/com/zmn/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hotfix test");
         System.out.println("master2 test!");
+        System.out.println("push test!");
     }
 }
